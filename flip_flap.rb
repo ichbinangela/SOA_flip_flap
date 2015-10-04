a76e4ad689bc7d46ffc9c49017e19f62828526ac
Angela <angela.hung@iss.nthu.edu.tw>
2015-10-05 02:09:47 +0800

    Adding methods

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,6 +1,14 @@
 require_relative 'tsv_buddy'
+require 'yaml'
 
 class FlipFlap
   # Do NOT create an initialize method
+  include TsvBuddy
+  def take_yaml(yml)
+    @data = YAML.load(yml)
+  end
 
+  def to_yml
+  	@data.to_yaml
+  end
 end
